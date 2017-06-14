81addfe3854c5917317d3ec0dbc5de8d51c40b25
Joosung Ahn <joosungahn@Joosungui-iMac.local>
2017-06-14 19:11:57 +0900

    Doing docs / colors.pug

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ app.set('view engine', 'pug')
 app.locals.pretty = true
 app.use('/', express.static('public'))
 
-app.get('/', function (req, res) {
-  res.render('room-setting')
-})
-app.get('/room-manage/', function (req, res) {
-  res.render('room-setting')
-})
-app.get('/docs/', function (req, res) {
-  res.render('docs/empty-page')
-})
+app.get('/', function (req, res){res.render('room-setting')})
+app.get('/room-manage/', function (req, res){res.render('room-setting')})
+
+// DOCS
+app.get('/docs/', function (req, res){res.render('docs/empty-page')})
+app.get('/docs/empty-page', function (req, res){res.render('docs/empty-page')})
+app.get('/docs/empty-page2', function (req, res){res.render('docs/empty-page2')})
+app.get('/docs/layout', function (req, res){res.render('docs/layout')})
+app.get('/docs/colors', function (req, res){res.render('docs/colors')})
+app.get('/docs/typography', function (req, res){res.render('docs/typography')})
+app.get('/docs/form-controls', function (req, res){res.render('docs/form-controls')})
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
